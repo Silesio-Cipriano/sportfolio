6f04b034aeba8b53c89b70805bfeac6260130c9f
Silésio Leudério Cipriano <silesioleuderiocipriano@hotmail.com>
2023-03-01 17:18:13 +0200

    Start: *header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,91 +1,66 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
+    <>
+      <div className=" text-textPrimary-500 p-8 flex justify-between items-center">
         <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src={'/images/home/sc.svg'}
+          width={42}
+          height={42}
+          alt="logo de escrito SC"
         />
-        <div className={styles.thirteen}>
-          <Image src="/thirteen.svg" alt="13" width={40} height={31} priority />
+
+        <nav>
+          <ul className="flex gap-10">
+            <li>
+              <Link href="">Inicio</Link>
+            </li>
+            <li>
+              <Link href="">Sobre</Link>
+            </li>
+            <li>
+              <Link href="">Projectos</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <div>
+          <ul className="flex gap-4">
+            <li>
+              <Link href="">
+                <Image
+                  src="/images/icons/social/Gmail.svg"
+                  width={45}
+                  height={45}
+                  alt="Gmail icon"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <Image
+                  src="/images/icons/social/Instagram.svg"
+                  width={45}
+                  height={45}
+                  alt="Instagram icon"
+                />
+              </Link>
+            </li>
+            <li>
+              <Link href="">
+                <Image
+                  src="/images/icons/social/Linkedin.svg"
+                  width={45}
+                  height={45}
+                  alt="LinkedLn icon"
+                />
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p className={inter.className}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </>
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      primary: {
+        500: '#B4DBEC',
+      },
+      secondary: {
+        500: '#130A0A',
+      },
+      textPrimary: {
+        500: '#ffffff',
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
